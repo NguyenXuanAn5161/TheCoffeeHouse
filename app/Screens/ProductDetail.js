1c3fa1dac070aeffee3b63c39a56ed08ca183f5c
Nguyen Xuan An <nguyenxuanan20021010@gmail.com>
2024-11-08 21:27:44 +0700

    refactor: finish screen product detail

--- app/Screens/ProductDetail.js
+++ app/Screens/ProductDetail.js
@@ -5,10 +5,10 @@ import {
   Image,
   ScrollView,
   Pressable,
+  Alert,
 } from "react-native";
 import React, { useState } from "react";
 import { colors, globalStyles } from "@/styles/globalStyles";
-import { SafeAreaView } from "react-native-safe-area-context";
 import {
   MaterialIcons,
   Ionicons,
@@ -33,109 +33,134 @@ const product = [
 ];
 
 const ProductDetail = () => {
-  const { img, title, price, discount, sizes } = product[0];
+  const { img, title, price, discount, sizes, id } = product[0];
   const [quantity, setQuantity] = useState(1);
   const [selectedSize, setSelectedSize] = useState(sizes[0]);
 
   const handleIncrement = () => setQuantity(quantity + 1);
   const handleDecrement = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
-
+  const handleAddCart = (id) => {
+    Alert.alert(`Thêm thành công! ${id}`);
+  };
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.contentContainer}>
         {/* Image */}
         <View
           style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
+            backgroundColor: colors.primary,
+            width: "100%",
+            justifyContent: "flex-end",
+            height: "30%",
           }}
         >
-          <Image source={img} style={styles.productImage} />
+          <View
+            style={{
+              backgroundColor: colors.white,
+              borderTopLeftRadius: 900,
+              borderTopRightRadius: 900,
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50%",
+            }}
+          >
+            <Image source={img} style={styles.productImage} />
+          </View>
         </View>
 
         {/* Title and Icons */}
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={styles.productTitle}>{title}</Text>
-          {/* Quantity and Add to Cart */}
-          <View style={styles.quantityContainer}>
-            <Pressable onPress={handleDecrement} style={styles.quantityButton}>
-              <FontAwesome6 name="minus" size={20} color={colors.primary} />
+        <View style={{ paddingHorizontal: 16, rowGap: 16 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text style={styles.productTitle}>{title}</Text>
+            {/* Quantity and Add to Cart */}
+            <View style={styles.quantityContainer}>
+              <Pressable
+                onPress={handleDecrement}
+                style={styles.quantityButton}
+              >
+                <FontAwesome6 name="minus" size={20} color={colors.primary} />
+              </Pressable>
+              <Text style={styles.quantityText}>{quantity}</Text>
+              <Pressable
+                onPress={handleIncrement}
+                style={styles.quantityButton}
+              >
+                <FontAwesome6 name="plus" size={20} color={colors.primary} />
+              </Pressable>
+            </View>
+          </View>
+          <Text style={styles.productDiscount}>{discount}</Text>
+          <View style={styles.iconsContainer}>
+            <Pressable style={[styles.iconButton]}>
+              <MaterialIcons name="share" size={24} color={colors.primary} />
             </Pressable>
-            <Text style={styles.quantityText}>{quantity}</Text>
-            <Pressable onPress={handleIncrement} style={styles.quantityButton}>
-              <FontAwesome6 name="plus" size={20} color={colors.primary} />
+            <Pressable style={[styles.iconButton]}>
+              <Ionicons name="heart" size={24} color={colors.primary} />
             </Pressable>
           </View>
-        </View>
-        <Text style={styles.productDiscount}>{discount}</Text>
-        <View style={styles.iconsContainer}>
-          <Pressable style={[styles.iconButton]}>
-            <MaterialIcons name="share" size={24} color={colors.primary} />
-          </Pressable>
-          <Pressable style={[styles.iconButton]}>
-            <Ionicons name="heart" size={24} color={colors.primary} />
-          </Pressable>
-        </View>
 
-        {/* Size Selection */}
-        <View style={styles.sizeContainer}>
-          <Text style={styles.sizeTitle}>Chọn Size:</Text>
-          <View style={styles.sizeOptions}>
-            {sizes.map((sizeOption, index) => {
-              // Logic thay đổi kích thước ảnh tùy theo size
-              const imageSize =
-                sizeOption.size === "S"
-                  ? 40
-                  : sizeOption.size === "M"
-                  ? 50
-                  : 60;
+          {/* Size Selection */}
+          <View style={styles.sizeContainer}>
+            <Text style={styles.sizeTitle}>Chọn Size:</Text>
+            <View style={styles.sizeOptions}>
+              {sizes.map((sizeOption, index) => {
+                // Logic thay đổi kích thước ảnh tùy theo size
+                const imageSize =
+                  sizeOption.size === "S"
+                    ? 40
+                    : sizeOption.size === "M"
+                    ? 50
+                    : 60;
 
-              return (
-                <Pressable
-                  key={index}
-                  onPress={() => setSelectedSize(sizeOption)}
-                  style={[
-                    styles.sizeButton,
-                    selectedSize.size === sizeOption.size &&
-                      styles.selectedSize,
-                  ]}
-                >
-                  <Image
-                    source={require("@/assets/images/icon_ly_cafe2.png")}
-                    style={{
-                      width: imageSize,
-                      height: imageSize,
-                      marginBottom: 8,
-                    }}
-                  />
-                  <Text>Size {sizeOption.size}</Text>
-                  <Text style={styles.selectedSizePrice}>
-                    {sizeOption.price}
-                  </Text>
-                </Pressable>
-              );
-            })}
+                return (
+                  <Pressable
+                    key={index}
+                    onPress={() => setSelectedSize(sizeOption)}
+                    style={[
+                      styles.sizeButton,
+                      selectedSize.size === sizeOption.size &&
+                        styles.selectedSize,
+                    ]}
+                  >
+                    <Image
+                      source={require("@/assets/images/icon_ly_cafe2.png")}
+                      style={{
+                        width: imageSize,
+                        height: imageSize,
+                        marginBottom: 8,
+                      }}
+                    />
+                    <Text>Size {sizeOption.size}</Text>
+                    <Text style={styles.selectedSizePrice}>
+                      {sizeOption.price}
+                    </Text>
+                  </Pressable>
+                );
+              })}
+            </View>
           </View>
-        </View>
 
-        <Pressable style={styles.addToCartButton}>
-          <FontAwesome5
-            name="shopping-cart"
-            size={24}
-            color={colors.white}
-            style={styles.shopping_card}
-          />
-          <Text style={styles.addToCartText}>Thêm vào giỏ hàng</Text>
-        </Pressable>
+          <Pressable
+            style={styles.addToCartButton}
+            onPress={() => handleAddCart(id)}
+          >
+            <FontAwesome5
+              name="shopping-cart"
+              size={24}
+              color={colors.white}
+              style={styles.shopping_card}
+            />
+            <Text style={styles.addToCartText}>Thêm vào giỏ hàng</Text>
+          </Pressable>
+        </View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -147,18 +172,18 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
   contentContainer: {
-    paddingHorizontal: 16,
     rowGap: 10,
   },
   productImage: {
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
     resizeMode: "contain",
+    bottom: 80,
+    zIndex: 1000,
   },
   productTitle: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 8,
   },
   iconsContainer: {
     flexDirection: "row",
@@ -198,11 +223,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#333",
     marginTop: 8,
-  },
-  productPrice: {
-    fontSize: 20,
-    color: "green",
-    marginBottom: 8,
   },
   productDiscount: {
     fontSize: 16,
