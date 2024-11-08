7da1b9a12a5069b4bab5d0fffecbd29e9ca8879d
Nguyen Xuan An <nguyenxuanan20021010@gmail.com>
2024-11-08 10:37:13 +0700

    refactor: safe area not working

--- App.js
+++ App.js
@@ -3,13 +3,22 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AppNavigator from "@components/AppNavigator";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
